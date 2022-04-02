697a27a87f6ed4fd5736657aa9cc630b92d7cbdb
DESKTOP-PBB3F4T\dtf11 <dtf114@naver.com>
2022-04-02 18:29:03 +0900

    feat(setup): 당장 필요한 폴더 구조 나누기 및 라우트 처리

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TestPage from './pages/TestPage/TestPage';
+import Gallery from './pages/Gallery/Gallery';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<TestPage />} />
+				<Route path="/AllGallery" element={<Gallery />} />
 			</Routes>
 		</BrowserRouter>
 	);
