d0b07727643ec7b4fa4bd48b8c0c732884d70aa8
JeongYunMi <tamd5971@gmail.com>
2022-04-13 17:47:54 +0900

    chore: 브랜치 설명을 위한 테스트 페이지 문구 변경

--- front/src/pages/TestPage/TestPage.jsx
+++ front/src/pages/TestPage/TestPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TestPage = () => {
-	return <div>반짝반짝 빛나는 테스트 페이지~!~!~!~!</div>;
+	return <div>반짝반짝 빛나는 테스트 페이지~!~!~!~!22.04.13</div>;
 };
 
 export default TestPage;
