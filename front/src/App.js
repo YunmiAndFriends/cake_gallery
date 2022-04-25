de46924cebd1cc6e5c717817983ab66778222be5
DESKTOP-PBB3F4T\dtf11 <dtf114@naver.com>
2022-04-25 16:37:19 +0900

    feat: 가게 페이지 라우트 연결

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import Cover from './pages/Main/cover';
 import Gallery from './pages/Gallery/Gallery';
 import Main from './pages/Main/main';
+import Store from './pages/Gallery/Store';
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				{/* <Route path="/" element={<Cover />} /> */}
 				<Route path="/" element={<Main />} />
 				<Route path="/AllGallery" element={<Gallery />} />
+				<Route path="/Store" element={<Store />} />
 			</Routes>
 		</BrowserRouter>
 	);
