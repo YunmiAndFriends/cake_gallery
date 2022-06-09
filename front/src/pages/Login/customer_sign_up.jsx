a690fd134b17973db47b2e4356733759989051e6
gpffh0001 <ekftn90@gmail.com>
2022-06-10 03:46:43 +0900

    feat: 로그인 onClick 이벤트 함수 추가

--- front/src/pages/Login/customer_sign_up.jsx
+++ front/src/pages/Login/customer_sign_up.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
 import {
 	Container,
@@ -13,8 +14,53 @@ import {
 } from './customer_signup_style';
 import SignUpInfo from './sign_up_info';
 import { IoMdPersonAdd } from 'react-icons/io';
+import { useState } from 'react';
+import { useMemo } from 'react';
 
 function signup_page() {
+	const [userInfo, setUserInfo] = useState({
+		userName: '',
+		hp: '',
+		email: '',
+		birthday: new Date(),
+		id: '',
+		pw: '',
+	});
+
+	const onChangeUserInfo = e => {
+		if (e.target.name === 'hp') {
+			let stValue = e.target.value;
+			for (let i = 0; i < stValue.length; i++) {
+				const num = Number(stValue[i]);
+				if (!Number.isInteger(num)) {
+					alert('숫자만 입력 가능합니다.');
+					return;
+				}
+			}
+		}
+
+		/**
+		 * 변수1 = indexOf 메소드를 통해서 @ 의 문자열 위치
+		 * 변수 1이 -1이면 탈락!
+		 * 변수2 = indexOf 메소드를 통해서 . 이 문자열 위치
+		 * 변수2가 -1이면 탈락!
+		 * 변수1 < 변수2 이면 탈락!!!!!!!!!!!!!!!!!!
+		 */
+
+		setUserInfo({
+			...userInfo,
+			[e.target.name]: e.target.value,
+		});
+		console.log('변경된 값 확인', e.target.name, userInfo[e.target.name]);
+	};
+
+	const checkPassWord = e => {
+		/**
+		 * if userInfo.pw !== e.target.value
+		 *  => 미안한데 비밀번호가 다름
+		 */
+	};
+
 	return (
 		<Container>
 			<ContentWrap>
@@ -25,16 +71,36 @@ function signup_page() {
 					<Title>손님 회원가입 페이지</Title>
 				</TitleDivWrap>
 				<ContainerWrap>
-					<SignUpInfo text="이름" />
-					<SignUpInfo text="전화번호" />
-					<SignUpInfo text="이메일" />
+					<SignUpInfo
+						text="이름"
+						name="userName"
+						data={userInfo.userName}
+						onChangefunc={onChangeUserInfo}
+					/>
+					<SignUpInfo
+						text="전화번호"
+						name="hp"
+						data={userInfo.hp}
+						onChangefunc={onChangeUserInfo}
+					/>
+					<SignUpInfo
+						text="이메일"
+						name="email"
+						data={userInfo.email}
+						onChangefunc={onChangeUserInfo}
+					/>
 					<InputDivWrap>
 						<Text>생일</Text>
-						<Input type="date" />
+						<Input type="date" name="birthday" onChange={onChangeUserInfo} max="2015-06-01" />
 					</InputDivWrap>
-					<SignUpInfo text="ID" />
-					<SignUpInfo text="비밀번호" />
-					<SignUpInfo text="비밀번호 확인" />
+					<SignUpInfo text="ID" name="id" data={userInfo.id} onChangefunc={onChangeUserInfo} />
+					<SignUpInfo
+						text="비밀번호"
+						name="pw"
+						data={userInfo.pw}
+						onChangefunc={onChangeUserInfo}
+					/>
+					<SignUpInfo text="비밀번호 확인" onChange={checkPassWord} />
 				</ContainerWrap>
 				<SubmitButton>회원가입</SubmitButton>
 			</ContentWrap>
--- front/src/pages/Login/login_page.jsx
+++ front/src/pages/Login/login_page.jsx
@@ -17,8 +17,21 @@ import {
 import LoginLogo from '../../assets/cake-piece.png';
 import { BiUser } from 'react-icons/bi';
 import { MdPassword } from 'react-icons/md';
+import { useState } from 'react';
 
 function login_page() {
+	const [loginId, setloginId] = useState('');
+	const [loginPw, setloginPw] = useState('');
+
+	const onChangeId = e => {
+		setloginId(e.target.value);
+	};
+	const onChangePw = e => {
+		setloginPw(e.target.value);
+	};
+
+	const onClickLogin = () => {};
+
 	let navigate = useNavigate();
 	function handleClick() {
 		navigate('/customer_signup');
@@ -34,13 +47,13 @@ function login_page() {
 			<Text>Welcome to Cake Gallery</Text>
 			<InputDivWrap>
 				<BiUser size={24} />
-				<Input />
+				<Input type="text" onChange={onChangeId} value={loginId} />
 			</InputDivWrap>
 			<InputDivWrap>
 				<MdPassword size={24} />
-				<Input type="password" />
+				<Input type="password" onChange={onChangePw} value={loginPw} />
 			</InputDivWrap>
-			<LoginButton>Login</LoginButton>
+			<LoginButton onClick={onClickLogin}>Login</LoginButton>
 			<JoinButtonWrap>
 				<JoinButton onClick={handleClick}>손님으로 회원가입</JoinButton>
 				<JoinButton onClick={signUpClick}> 사장님으로 회원가입</JoinButton>
