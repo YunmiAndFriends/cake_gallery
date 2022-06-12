6f847dfa4122d43d3dbe7bd1d654ab87453652fe
maaanzi <mz151@naver.com>
2022-06-13 00:06:41 +0900

    feat: 개인/사장님 마이페이지 틀

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Cover from './pages/Main/cover';
 import Gallery from './pages/Gallery/Gallery';
 import Main from './pages/Main/main';
+import Mypage from './pages/Main/mypage';
+import _Mypage from './pages/Main/_mypage';
 
 function App() {
 	return (
@@ -9,6 +11,9 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Cover />} />
 				<Route path="/main" element={<Main />} />
+				<Route path="/mypage" element={<Mypage />} />
+				<Route path="/_mypage" element={<_Mypage />} />
+
 				<Route path="/AllGallery" element={<Gallery />} />
 			</Routes>
 		</BrowserRouter>
--- front/src/pages/Main/main_style.jsx
+++ front/src/pages/Main/main_style.jsx
@@ -3,25 +3,52 @@ import banner from '../../assets/banner.png';
 
 const Img = styled.div`
 	background-image: url(${banner});
+	background-size: contain;
 	width: 100%;
-	height: 80%;
+	height: 328px;
 `;
-// const Frame = styled.div`
-// 	display: flex;
-// 	flex-direction: row;
-// `;
+const Img2 = styled.img`
+	width: 400px;
+	height: 240px;
+`;
+const Img3 = styled.img`
+	width: 560px;
+	height: 240px;
+	justify-content: center;
+`;
+const Frame = styled.div`
+	margin-top: 10vh;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+`;
 const Gallery = styled.div`
+	padding-right: 10vh;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+`;
+
+const Body = styled.div`
+	display: flex;
+	flex-direction: column;
+	text-align: center;
+	justify-content: center;
+	font-size: 130%;
 `;
 const G_Text = styled.div``;
 const G2_Text = styled.div``;
 const G_Button = styled.button``;
 const G_Info = styled.div``;
 
-const R_Text = styled.div``;
-const R_Info = styled.div``;
+const R_Text = styled.div`
+	margin-top: 48px;
+	margin-bottom: 32px;
+`;
+const R_Info = styled.div`
+	align-items: center;
+`;
 
-export { Img, Gallery, G_Text, G2_Text, G_Button, G_Info, R_Text, R_Info };
+export { Img, Img2, Img3, Gallery, Body, Frame, G_Text, G2_Text, G_Button, G_Info, R_Text, R_Info };
