e8266738b52203f02a3c96f09692d91bf310ff95
maaanzi <mz151@naver.com>
2022-06-15 03:13:46 +0900

    feat: 메인 들어가기전 커버페이지

--- front/src/pages/Main/cover.jsx
+++ front/src/pages/Main/cover.jsx
@@ -5,21 +5,27 @@ import { Link } from 'react-router-dom';
 const click = () => {
 	document.location.href('/main');
 };
+const click2 = () => {
+	document.location.href('/login');
+};
 
 const cover = () => {
 	return (
 		<Content>
-			<Header>
+			{/* <Header>
 				<Menu>
 					<Mypage>Mypage</Mypage>
 					<Login>Login</Login>
 				</Menu>
-			</Header>
+			</Header> */}
 			<Body>
 				<Text>특별한 날 소중함을 더해주는 </Text>
 				<Text2>하나뿐인 케이크</Text2>
 				<Link to="/main">
 					<Button onClick={click}>구경하러 가기</Button>
+				</Link>
+				<Link to="/login">
+					<Button onClick={click2}>Login</Button>
 				</Link>
 			</Body>
 		</Content>
