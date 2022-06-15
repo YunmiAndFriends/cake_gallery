5896f0c803f622be1bfb4fac0ab5513663227b52
DESKTOP-PBB3F4T\dtf11 <dtf114@naver.com>
2022-06-15 13:53:07 +0900

    feat: 후기 내용 변경

--- front/src/pages/Review/AllReviewData.jsx
+++ front/src/pages/Review/AllReviewData.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { AllReviewWrap, ThumbnailImg, Storename, Review } from './AllReviewData.style';
+import { Link } from 'react-router-dom';
 
 const AllReviewData = ({ id, title, thumbnail, writer }) => {
 	return (
-		<AllReviewWrap>
-			<ThumbnailImg src={thumbnail} />
-			<Storename>{title}</Storename>
-			<Review>{writer}</Review>
-		</AllReviewWrap>
+		<Link style={{ textDecoration: 'none', color: 'black' }} to="/ReviewDetailes">
+			<AllReviewWrap>
+				<ThumbnailImg src={thumbnail} />
+				<Storename>{title}</Storename>
+				<Review>{writer}</Review>
+			</AllReviewWrap>
+		</Link>
 	);
 };
 
